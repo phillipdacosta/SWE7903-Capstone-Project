ffd1ef99dc406d62e36526e6582403f86d6cbf96
clincol2 <43626533+clincol2@users.noreply.github.com>
2019-10-17 13:09:12 -0400

    Add files via upload

--- src/app/project.model.ts
+++ src/app/project.model.ts
@@ -1,14 +1,29 @@
-import { Team } from './team.model';
 
 export class Project {
 
-    name: string;
-    team: Team;
-    testEnv: any;
+    _go_live_date: string;
+    _groject_RYG_Color: string;
+    _last_Updated: string;
+    _project_Manager: string;
+    _project_Manager_ID: string
+    _project_Start_Date: string;
+    _project_Status: string;
+    _project_Title: string;
+    _project_Work_Type: string;
+    _id: string;
 
-    constructor(name: string = "", team: Team = new Team(), env: any = ""){
-        this.name = name;
-        this.team = team;
-        this.testEnv = env;
+
+    constructor(date: string = "", color: any = "",update_time: any = "",proj_man: any = "",proj_man_id: any = "",start_date: any = "",status: any = "",title: any = "",work_type: any = "",id_db: any = "") {
+        this._go_live_date = date;
+        this._groject_RYG_Color = color;
+        this._last_Updated = update_time;
+        this._project_Manager = proj_man;
+        this._project_Manager_ID = proj_man_id;
+        this._project_Start_Date = start_date;
+        this._project_Status = status;
+        this._project_Title = title;
+        this._project_Work_Type = work_type
+        this._id = id_db;
+
     }
 }
